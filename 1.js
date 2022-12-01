75d53ae9c928c43a4db7856fb208cf33090c06d6
jram930 <jared.ramsey@heap.io>
2022-12-01 12:06:57 -0500

    Day 1

---
+++ 1.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const readline = require('readline');
+  
+
+const file = readline.createInterface({
+    input: fs.createReadStream('1.txt'),
+    output: process.stdout,
+    terminal: false
+});
+  
+
+let elf = 0;
+let counter = 0;
+let max = -1;
+let sum = 0;
+
+file.on('line', (line) => {
+    if(line === '') {
+      counter++;
+      console.log(`Elf ${counter} has ${sum}`);
+      if(sum > max) {
+        elf = counter;
+        max = sum;
+        console.log(`New max of ${max}`);
+      }
+      sum = 0;
+    } else {
+      sum += +line;
+    }
+});
+
+setTimeout(() => {
+  console.log(elf);
+}, 5000);
---
+++ 2.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const readline = require('readline');
+  
+
+const file = readline.createInterface({
+    input: fs.createReadStream('1.txt'),
+    output: process.stdout,
+    terminal: false
+});
+  
+
+let counter = 0;
+let max = 0;
+let sum = 0;
+let second = 0;
+let third = 0;
+
+file.on('line', (line) => {
+    if(line === '') {
+      counter++;
+      console.log(`Elf ${counter} has ${sum}`);
+      if(sum > max) {
+        third = second;
+        second = max;
+        max = sum;
+      } else if (sum > second) {
+        third = second;
+        second = sum;
+      } else if(sum > third) {
+        third = sum;
+      }
+      sum = 0;
+    } else {
+      sum += +line;
+    }
+});
+
+setTimeout(() => {
+  console.log(max + second + third);
+}, 5000);
